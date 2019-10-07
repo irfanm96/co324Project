78a7e06b520d3dc7b8f0f071e256f191546f4a8b
irfanm96 <e15138@ce.pdn.ac.lk>
2019-10-07 18:30:18 +0530

    emergency updates

--- new/MulticastSocketServer.java
+++ new/MulticastSocketServer.java
@@ -1,45 +1,34 @@
-package multicast;
-
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-
-public class MulticastSocketServer extends Thread {
-
+ 
+public class MulticastSocketServer {
+     
     final static String INET_ADDR = "224.0.0.3";
     final static int PORT = 8888;
-
-    @Override
-    public void run() {
+ 
+    public static void main(String[] args) throws UnknownHostException, InterruptedException {
         // Get the address that we are going to connect to.
-        InetAddress addr = null;
-        try {
-            addr = InetAddress.getByName(INET_ADDR);
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-
+        InetAddress addr = InetAddress.getByName(INET_ADDR);
+      
         // Open a new DatagramSocket, which will be used to send the data.
         try (DatagramSocket serverSocket = new DatagramSocket()) {
-            int i = 0;
-            while (true) {
-                String msg = "Sent message from irfan " + i;
-
+            for (int i = 0; i < 5; i++) {
+                String msg = "Sent message no " + i;
+ 
                 // Create a packet that will contain the data
                 // (in the form of bytes) and send it.
                 DatagramPacket msgPacket = new DatagramPacket(msg.getBytes(),
                         msg.getBytes().length, addr, PORT);
                 serverSocket.send(msgPacket);
-
-                System.out.println("Server sent packet with msg: " + msg + '\n');
+      
+                System.out.println("Server sent packet with msg: " + msg);
                 Thread.sleep(500);
-                i = (i + 1) % 10;
             }
-        } catch (IOException | InterruptedException ex) {
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
-
     }
 }
